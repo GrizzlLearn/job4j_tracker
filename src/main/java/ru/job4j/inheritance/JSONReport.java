61d91d5f8ff75bac181b0b7b6ec4521c2f86593f
GrizzlLearn <dylebedev.learning@gmail.com>
2022-05-02 14:59:33 +0300

    4. Переопределение [#174890]

---
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -0,0 +1,15 @@
+package ru.job4j.inheritance;
+
+public class JSONReport extends TextReport {
+    public String generate(String name, String body) {
+        String ln = System.lineSeparator();
+        String sName = String.format("\"%s\"", name);
+        String sBody = String.format("\"%s\"", body);
+        return "{" + ln
+                + "\t\"name\" : " + sName + "," + ln
+                + "\t\"body\" : " + sBody + ln
+                + "}";
+    }
+}
+
+
