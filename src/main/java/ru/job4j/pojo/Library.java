aa83d24b487157935c3350c64827fab3e25091ab
GrizzlLearn <dylebedev.learning@gmail.com>
2022-05-02 17:24:18 +0300

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,38 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book book1 = new Book("Clean code", 464);
+        Book book2 = new Book("Град обреченный", 408);
+        Book book3 = new Book("Первый Закон", 1671);
+        Book book4 = new Book("Тёмная Башня", 4316);
+
+        Book[] books = new Book[4];
+        books[0] = book1;
+        books[1] = book2;
+        books[2] = book3;
+        books[3] = book4;
+
+        for (int i = 0; i < books.length; i++) {
+            System.out.println(books[i].getName());
+        }
+
+        System.out.println("---");
+
+        Book tmp = books[0];
+        books[0] = books[3];
+        books[3] = tmp;
+
+        for (int i = 0; i < books.length; i++) {
+            System.out.println(books[i].getName());
+        }
+
+        System.out.println("---");
+
+        for (Book book: books) {
+            if (book.getName().equals("Clean code")) {
+                System.out.println("Название: " + book.getName() + ". Количество страниц: " + book.getPageCount() + ".");
+            }
+        }
+    }
+}
