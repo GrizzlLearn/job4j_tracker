d29a205abf8d2e141c96e30575ac4d705f9487fc
GrizzlLearn <dylebedev.learning@gmail.com>
2022-05-02 18:52:45 +0300

    6. Date. Отображение даты. [#310017 #296288] fix

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,9 +1,12 @@
 package ru.job4j.tracker;
 
+import java.time.format.DateTimeFormatter;
+
 public class StartUI {
 
     public static void main(String[] args) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         Item item = new Item(0, "first");
-        System.out.println(item.getCreated());
+        System.out.println(item.getCreated().format(formatter));
     }
 }
