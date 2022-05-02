46c08ceaf8ebcd70f4eb22132a019a54c3048740
GrizzlLearn <dylebedev.learning@gmail.com>
2022-05-02 23:10:24 +0300

    6. Метод замены заявки. Tracker.replace [#211748]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -13,15 +13,20 @@ public class Tracker {
         return item;
     }
 
-    public Item findById(int id) {
-        Item rsl = null;
+    private int indexOf(int id) {
+        int rsl = -1;
         for (int i = 0; i < size; i++) {
             if (items[i].getId() == id) {
-                rsl = items[i];
+                rsl = i;
                 break;
             }
         }
         return rsl;
+    }
+
+    public Item findById(int id) {
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
     }
 
     public Item[] findByName(String name) {
@@ -47,5 +52,16 @@ public class Tracker {
             }
         }
         return Arrays.copyOf(rsl, tmp);
+    }
+
+    public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        if (index == -1) {
+            return false;
+        }
+        int tmpId = items[index].getId();
+        items[index] = item;
+        items[index].setId(tmpId);
+        return true;
     }
 }
