157bff2fb584dbc0bef7150a6879e0879b541e4d
GrizzlLearn <dylebedev.learning@gmail.com>
2022-05-04 23:11:43 +0300

    1. Чтение из консоли. Класс Scanner. [#179514] fix

--- src/main/java/ru/job4j/io/MagicBall.java
+++ src/main/java/ru/job4j/io/MagicBall.java
@@ -6,14 +6,12 @@ import java.util.Scanner;
 public class MagicBall {
 
      public static class Answer {
-         public int random() {
-             return new Random().nextInt(3);
-         }
+         int random = new Random().nextInt(3);
 
          public String magicAnswer(String question) {
-            if (random() == 0) {
+            if (random == 0) {
                 return "Ответ на твой вопрос: " + question + " -> Да";
-            } else if (random() == 1) {
+            } else if (random == 1) {
                 return "Ответ на твой вопрос: " + question + " -> Нет";
             } else {
                 return "Ответ на твой вопрос: " + question + " -> Может быть";
