714549cefb661f54a0eafaa950cbbf17bb1af028
GrizzlLearn <dylebedev.learning@gmail.com>
2022-05-20 16:14:58 +0300

    0.6. Error - исключения, связанные с работой виртуальной машины [#219370 #302787] fix

--- src/main/java/ru/job4j/ex/FactRec.java
+++ src/main/java/ru/job4j/ex/FactRec.java
@@ -3,7 +3,7 @@ package ru.job4j.ex;
 public class FactRec {
     public static int calc(int n) {
         if (n < 0) {
-            return -1;
+            throw new IllegalArgumentException("N could not be less then 0");
         }
 
         return n == 0 || n == 1 ? 1 : calc(n - 1) * n;
