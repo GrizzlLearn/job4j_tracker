437fe6e762bfdbc182373b3353227c350bc589af
GrizzlLearn <dylebedev.learning@gmail.com>
2022-06-20 12:21:15 +0300

    0. Map, HashMap [#212652 #313733] fix

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,11 +7,15 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> users = new HashMap<>();
         users.put("qwer@a.com", "Qwer");
+        users.put("qwer@a.com", "Qwer");
         users.put("qwert@a.com", "Qwert");
         users.put("qwerty@a.com", "Qwerty");
+        users.put("qwerty@a.com", "Qwerty");
         users.put("qwertyu@a.com", "Qwertyu");
-        for (Map.Entry<String, String> entry : users.entrySet()) {
-            System.out.println("Email: " + entry.getKey() + " -> Username: "  + entry.getValue());
+        users.put("qwertyu@a.com", "Qwertyu");
+        users.put("qwertyu@a.com", "Qwertyu");
+        for (String key : users.keySet()) {
+            System.out.println("Email: " + key + " -> Username: "  + users.get(key));
         }
     }
 }
