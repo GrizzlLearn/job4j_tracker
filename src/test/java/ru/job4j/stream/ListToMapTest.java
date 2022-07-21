024d8b3eebbea17d9a16d6bcf941c3ba9050d639
GrizzlLearn <dylebedev.learning@gmail.com>
2022-07-21 23:20:18 +0300

    4. Преобразование List в Map. [#110226]

---
+++ src/test/java/ru/job4j/stream/ListToMapTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.stream;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+import java.util.*;
+
+public class ListToMapTest {
+
+    @Test
+    public void whenInputListNotHaveDuplicate() {
+        List<Student> list = Arrays.asList(
+                new Student(30, "Ivanov"),
+                new Student(40, "Petrov"),
+                new Student(50, "Sidorov")
+        );
+        Map<String, Student> result = ListToMap.convert(list);
+        Map<String, Student> expected = new LinkedHashMap<>();
+        expected.put("Ivanov", new Student(30, "Ivanov"));
+        expected.put("Petrov", new Student(40, "Petrov"));
+        expected.put("Sidorov", new Student(50, "Sidorov"));
+        assertThat(expected).isEqualTo(result);
+    }
+
+    @Test
+    public void whenInputListHaveDuplicate() {
+        List<Student> list = Arrays.asList(
+                new Student(30, "Ivanov"),
+                new Student(40, "Petrov"),
+                new Student(50, "Ivanov"),
+                new Student(60, "Sidorov"),
+                new Student(70, "Petrov")
+        );
+        Map<String, Student> result = ListToMap.convert(list);
+        Map<String, Student> expected = new LinkedHashMap<>();
+        expected.put("Ivanov", new Student(30, "Ivanov"));
+        expected.put("Petrov", new Student(40, "Petrov"));
+        expected.put("Sidorov", new Student(60, "Sidorov"));
+        assertThat(expected).isEqualTo(result);
+    }
+}
