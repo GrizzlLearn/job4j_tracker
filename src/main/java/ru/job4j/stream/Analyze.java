9ad9e7c36e73a983c8332cd78bd92d20822b5d7c
GrizzlLearn <dylebedev.learning@gmail.com>
2022-08-15 23:08:20 +0300

    WIP: 2. Аттестация [#49777] averageScore done

---
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -0,0 +1,30 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Analyze {
+    public static double averageScore(Stream<Pupil> stream) {
+        return stream
+                .flatMap(p -> p.subjects().stream())
+                .mapToInt(Subject::score)
+                .average()
+                .orElse(Double.NaN);
+    }
+
+    public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
+        return List.of();
+    }
+
+    public static List<Tuple> averageScoreBySubject(Stream<Pupil> stream) {
+        return List.of();
+    }
+
+    public static Tuple bestStudent(Stream<Pupil> stream) {
+        return null;
+    }
+
+    public static Tuple bestSubject(Stream<Pupil> stream) {
+        return null;
+    }
+}
