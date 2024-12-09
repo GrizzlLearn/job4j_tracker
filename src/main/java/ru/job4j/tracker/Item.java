9f80782e12860c1f2d26ad1605d87d8c1a9ca139
GrizzlLearn <dylebedev.learning@gmail.com>
2024-12-09 22:09:47 +0300

    1. Lombok. Настройка IDEA [#504888]: done

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,9 +1,11 @@
 package ru.job4j.tracker;
 
+import lombok.Data;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
+@Data
 public class Item {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
     private int id;
@@ -18,30 +20,6 @@ public class Item {
         this.name = name;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalDateTime getCreated() {
-        return created;
-    }
-
-    public void setCreated(LocalDateTime created) {
-        this.created = created;
-    }
-
     @Override
     public String toString() {
         return "Item{"
@@ -50,31 +28,5 @@ public class Item {
                 + '\''
                 + ", created=" + created.format(FORMATTER)
                 + '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof Item item)) {
-            return false;
-        }
-
-        if (id != item.id) {
-            return false;
-        }
-        if (!Objects.equals(name, item.name)) {
-            return false;
-        }
-        return Objects.equals(created, item.created);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id;
-        result = 31 * result + name.hashCode();
-        result = 31 * result + created.hashCode();
-        return result;
     }
 }
